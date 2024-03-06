0eaf8f01ccdf7b67dfe9ebe2747bc3467c1f5d9b
LuisVDT <luisvital.de@gmail.com>
2024-03-06 17:02:58 -0600

    Migracion tabla empleados

---
+++ app/Http/Controllers/ModeloEmpleadoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\modeloEmpleado;
+use Illuminate\Http\Request;
+
+class ModeloEmpleadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(modeloEmpleado $modeloEmpleado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(modeloEmpleado $modeloEmpleado)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, modeloEmpleado $modeloEmpleado)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(modeloEmpleado $modeloEmpleado)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_03_06_225717_create_tabla_empleados.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tabla_empleados', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellidoP');
+            $table->string('apellidoM');
+            $table->string('puesto');
+            $table->string('departamento');
+            $table->date('fecha_nacimiento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tabla_empleados');
+    }
+};
